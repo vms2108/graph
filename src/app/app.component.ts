28ca20fd52e8be1ec535024da6b5f962b76a22ce
mikhail <vms2108@mail.ru>
2018-08-15 17:30:18 +0300

    import angular material and start calculate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public fg: FormGroup = new FormGroup({
+    costApartaments: new FormControl(),
+    datesFrom: new FormControl(),
+    datesTo: new FormControl(),
+    cashMonth: new FormControl(),
+    costRental: new FormControl(),
+    cash: new FormControl(),
+    percentCostChange: new FormControl(),
+    percentSalaryChange: new FormControl(),
+    percentCredit: new FormControl(),
+  });
+  public termCopy = 0;
+  public currentDate = new Date();
+  constructor(private fb: FormBuilder) {}
+ ngOnInit() {
+  this.fg = this.fb.group({
+    costApartaments: [null],
+    datesFrom: this.currentDate,
+    datesTo: this.currentDate,
+    salary: [null],
+    cashMonth: [null],
+    costRental: [null],
+    percentCostChange: [null],
+    percentSalaryChange: [null],
+    percentCredit: [null],
+    cash: [null]
+  });
+  this.fg.valueChanges.subscribe(() => {
+    this.getResult();
+  });
+ }
+
+  getResult() {
+    const value = this.fg.value;
+    let remain = value.costApartaments - value.cash;
+    let cashMonth = value.cashMonth;
+    let i = 0;
+    while (remain > 0) {
+      i++;
+      remain -= cashMonth;
+      if ( i % 12 === 0) {
+        cashMonth = cashMonth * value.percentSalaryChange;
+      }
+      console.log(i);
+    }
+    this.termCopy = i;
+  }
+
+  get termCredit() {
+    return 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
