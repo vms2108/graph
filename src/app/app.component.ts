e9184bb167c08bb88d1a581f43930759326fbaa0
mikhail <vms2108@mail.ru>
2018-08-21 14:04:37 +0300

    update somethink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { chunk } from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -18,48 +19,72 @@ export class AppComponent implements OnInit {
     percentSalaryChange: new FormControl(),
     percentCredit: new FormControl(),
   });
+  public sections: any[] = [];
   public termCopy = 0;
   public currentDate = new Date();
   constructor(private fb: FormBuilder) {}
  ngOnInit() {
   this.fg = this.fb.group({
-    costApartaments: [null],
-    datesFrom: this.currentDate,
-    datesTo: this.currentDate,
-    salary: [null],
-    cashMonth: [null],
-    costRental: [null],
-    percentCostChange: [null],
-    percentSalaryChange: [null],
-    percentCredit: [null],
-    cash: [null]
+    cost_apartaments: 5,
+    percent_cost_change: 10,
+
+    percent_credit: 12,
+    term_credit: 10,
+
+    cash: 500,
+    salary: 40,
+    spending: 20,
+    percent_salary_change: 15,
+    percent_spending_change: 15,
+
+    cost_rental: 20,
+    percent_cost_rental_change: 10
   });
+  this.getResult();
   this.fg.valueChanges.subscribe(() => {
     this.getResult();
   });
  }
 
+ afterYear(value, persent) {
+  return value * (1 + persent / 100);
+ }
+
+ getCashMonth(salary, spending, cost_rental) {
+  return salary - spending - cost_rental;
+ }
+
  getIf(value) {
-  return !value.costApartaments || !value.cashMonth || !value.percentSalaryChange || !value.percentCostChange || !value.cash;
+  return !value.cost_apartaments || !value.cash || !value.salary;
  }
 
   getResult() {
     const value = this.fg.value;
-    if (this.getIf(value)) {
-      return;
-    }
-    let remain = value.costApartaments - value.cash;
-    let cashMonth = value.cashMonth;
-    let costApartaments = value.costApartaments;
-    const percentSalaryChange = 1 + value.percentSalaryChange / 100;
-    const percentCostChange = 1 + value.percentCostChange / 100;
+    // if (this.getIf(value)) {
+    //   return;
+    // }
+    console.log(value);
+    let cost_apartaments = value.cost_apartaments * 1000000;
+    let salary = value.salary * 1000;
+    let spending = value.spending * 1000;
+    let cost_rental = value.cost_rental * 1000;
+    const cash = value.cash * 1000;
+    const start_remain = cost_apartaments - cash;
+    let remain = start_remain;
+    let cash_month = this.getCashMonth(salary, spending, cost_rental);
     let i = 0;
     while (remain > 0) {
       i++;
-      remain = (costApartaments - value.cash) - cashMonth * i;
+      remain = start_remain - (cost_apartaments - value.cash) - cash_month * i;
       if ( i % 12 === 0) {
-        cashMonth = cashMonth * percentSalaryChange;
-        costApartaments = costApartaments * percentCostChange;
+        cost_apartaments = this.afterYear(cost_apartaments, value.percent_cost_change);
+        salary = this.afterYear(salary, value.percent_salary_change);
+        spending = this.afterYear(spending, value.percent_spending_change);
+        cost_rental = this.afterYear(cost_rental, value.percent_cost_rental_change);
+        cash_month = this.getCashMonth(salary, spending, cost_rental);
+      }
+      if ( i >= 200 ) {
+        return;
       }
     }
     this.termCopy = i;
